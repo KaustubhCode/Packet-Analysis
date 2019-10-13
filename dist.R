af946c405c703b82a4223c61e286800b779a3f18
Kaustubh <kaustubh698@gmail.com>
2019-10-13 23:31:03 +0530

    Added R files

---
+++ dist.R
@@ -0,0 +1,26 @@
+
+df <- read.csv("/home/kaustubh/Desktop/COL334/Packet-Analysis/inter_arrival_3.csv", header=FALSE)
+library("fitdistrplus")
+colnames(df) <- "X"
+plotdist(df$X, histo=TRUE, demp=TRUE)
+
+par(mfrow=c(2,2))
+fe <- fitdist(df$X, "exp")
+fe
+denscomp(list(fe), legendtext=c("exp"))
+cdfcomp(list(fe), legendtext=c("exp"))
+qqcomp(list(fe), legendtext=c("exp"))
+
+par(mfrow=c(2,2))
+fe <- fitdist(df$X, "norm")
+fe
+denscomp(list(fe), legendtext=c("norm"))
+cdfcomp(list(fe), legendtext=c("norm"))
+qqcomp(list(fe), legendtext=c("norm"))
+
+par(mfrow=c(2,2))
+fe <- fitdist(df$X, "lnorm")
+fe
+denscomp(list(fe), legendtext=c("lnorm"))
+cdfcomp(list(fe), legendtext=c("lnorm"))
+qqcomp(list(fe), legendtext=c("lnorm"))
