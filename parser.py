183b7e7d72998f21fe8a14da489b070cb979ee67
Kaustubh <kaustubh698@gmail.com>
2019-10-13 14:56:39 +0530

    Completed till Q8

--- parser.py
+++ parser.py
@@ -7,9 +7,9 @@ def cleanData(df):
 	return df
 
 def getTCP(df):
-	df_tcp = df.loc[df["Protocol"] ==  "TCP"]
-	df_tcp["Source Port"] = df_tcp['Info'].apply(lambda x: int(x.split()[0]))
-	df_tcp['Destination Port'] = df_tcp['Info'].apply(lambda x: int(x.split()[2]))
+	df_tcp = df.loc[df["Protocol"] ==  "TCP"].copy()
+	df_tcp.loc[:,'Source Port'] = df_tcp['Info'].apply(lambda x: int(x.split()[0]))
+	df_tcp.loc[:,'Destination Port'] = df_tcp['Info'].apply(lambda x: int(x.split()[2]))
 	return df_tcp
 
 def getFTP(df):	
@@ -28,7 +28,7 @@ def getEnd(df):
 def getNums(df):
 	df_syn = getSyn(df)
 	# Below step redundant
-	# df_syn = df_syn.drop_duplicates(subset=['Info']) 
+	df_syn = df_syn.drop_duplicates(subset=['Info']) 
 	# Ack sent again (Considering that whole info can never match for a new connection)
 	return (df_syn['Source'].drop_duplicates().size,df_syn['Destination'].drop_duplicates().size)
 
@@ -85,7 +85,7 @@ def plotConnections(df):
 # Q5
 def connectionBytes(df):
 	df_merge = getConnections(df)[["Source","Destination","Source Port","Destination Port","Connection Time"]]
-	df_group = getTCP(df).groupby(["Source","Destination","Source Port","Destination Port"],sort=False)
+	df_group = getTCP(df).groupby(["Source","Destination","Source Port","Destination Port"])
 	df_length = {name:group["Length"].sum() for name,group in df_group}
 
 	df_final = {}
@@ -98,13 +98,24 @@ def connectionBytes(df):
 	Y = []
 	Z = []
 	for i in df_final:
-		X.append(df[i][0])
-		Y.append(df[i][1])
-		Z.append(df[i][2])
+		X.append(df_final[i][0])
+		Y.append(df_final[i][1])
+		Z.append(df_final[i][2])
+	X = np.array(X)
+	Y = np.array(Y)
+	Z = np.array(Z)
+	sort = X.argsort()
+	X = X[sort]
+	Y = Y[sort]
+	Z = Z[sort]
 
-	plt.plot(X,Y,'r')
-	plt.plot(X,Y,'g')
+
+	plt.scatter(X[:-2],Y[:-2],c='r')
+	# plt.scatter(X[:-2],Z[:-2],c='g')
+	# plt.scatter(X[:-2],Y[:-2] + Z[:-2],c='b')
 	plt.show()
+
+	#To implement PEARSONS
 
 # Q6
 def plotInterArrival(df):
@@ -122,6 +133,43 @@ def plotInterArrival(df):
 	plt.plot(inter_arrival,cumu)
 	plt.show()
 
+# Q7
+def plotPacketInterArrival(df):
+	df_tcp = getTCP(df)
+	df_to_server = df_tcp[df_tcp["Destination Port"] == 21]
+	packet_conn_times = df_to_server["Time"].to_numpy()
+	packet_inter_arrival = np.diff(packet_conn_times)
+
+	cumu = np.cumsum(packet_inter_arrival/packet_inter_arrival.sum())
+
+	print("Mean of inter arrival times:",packet_inter_arrival.mean())
+	print("Median of inter arrival times:",np.median(packet_inter_arrival))
+
+	packet_inter_arrival.sort()
+	print(packet_inter_arrival[-10:])
+	# Remove last two values as outliers - very large
+	plt.plot(packet_inter_arrival,cumu)
+	plt.show()
+
+# Q7
+def plotPacketLengths(df):
+	df_tcp = getTCP(df)
+	df_incoming = df_tcp[df_tcp["Destination Port"] != 21]
+	df_outgoing = df_tcp[df_tcp["Destination Port"] == 21]
+	packet_incoming = df_incoming["Length"].to_numpy()
+	packet_outgoing = df_outgoing["Length"].to_numpy()
+
+	cumu_incoming = np.cumsum(packet_incoming/packet_incoming.sum())
+	cumu_outgoing = np.cumsum(packet_outgoing/packet_outgoing.sum())
+
+	packet_incoming.sort()
+	packet_outgoing.sort()
+	# Remove last two values as outliers - very large
+	plt.plot(packet_incoming,cumu_incoming,'r')
+	plt.plot(packet_outgoing,cumu_outgoing,'b')
+	plt.show()
+
+
 if __name__ == "__main__":
 	df1 = cleanData(pd.read_csv("lbnl.anon-ftp.03-01-11.csv"))
 	df2 = cleanData(pd.read_csv("lbnl.anon-ftp.03-01-14.csv"))
@@ -132,4 +180,6 @@ if __name__ == "__main__":
 	# plotFlows(df1)
 	# plotConnections(df1)
 	# plotInterArrival(df1)
-	connectionBytes(df1)
+	# connectionBytes(df1)
+	# plotPacketInterArrival(df1)
+	plotPacketLengths(df1)
